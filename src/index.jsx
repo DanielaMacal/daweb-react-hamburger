c63fb0d9c873c017384826d9ae345c61a73317ba
Daniela Máčalová <Dan.Macalova@gmail.com>
2021-05-15 17:21:45 +0200

    3.úkol

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,11 @@ import './style.css';
 
 const MenuItem = (props) => {
   return (
-    <a onClick={props.onSelect} href="#" className="menu-item">
+    <a
+      onClick={() => props.onSelect(props.text)}
+      href="#"
+      className="menu-item"
+    >
       {props.text}
     </a>
   );
@@ -12,13 +16,15 @@ const MenuItem = (props) => {
 
 const App = () => {
   const [menuOpened, setMenuOpened] = useState(false);
+  const [pageTitle, setPageTitle] = useState('Domů');
 
   const handleOpened = () => {
     setMenuOpened(!menuOpened);
   };
 
-  const handleSelectItem = () => {
+  const handleSelectItem = (page) => {
     setMenuOpened(false);
+    setPageTitle(page);
   };
 
   return (
@@ -36,10 +42,50 @@ const App = () => {
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
 };
+
+// const MenuItem = (props) => {
+//   return (
+//     <a onClick={props.onSelect} href="#" className="menu-item">
+//       {props.text}
+//     </a>
+//   );
+// };
+
+// const App = () => {
+//   const [menuOpened, setMenuOpened] = useState(false);
+
+//   const handleOpened = () => {
+//     setMenuOpened(!menuOpened);
+//   };
+
+//   const handleSelectItem = () => {
+//     setMenuOpened(false);
+//   };
+
+//   return (
+//     <>
+//       <header>
+//         <div className={menuOpened === true ? 'menu' : 'menu menu--closed'}>
+//           <button onClick={handleOpened} className="menu__btn"></button>
+//           <div className="menu__items">
+//             <MenuItem onSelect={handleSelectItem} text="Domů" />
+//             <MenuItem onSelect={handleSelectItem} text="Naše nabídka" />
+//             <MenuItem onSelect={handleSelectItem} text="Náš tým" />
+//             <MenuItem onSelect={handleSelectItem} text="Blog" />
+//             <MenuItem onSelect={handleSelectItem} text="Kontakt" />
+//           </div>
+//         </div>
+//       </header>
+//       <main>
+//         <h1>Domů</h1>
+//       </main>
+//     </>
+//   );
+// };
 
 render(<App />, document.querySelector('#app'));
